c4f44866bd867a519bd4f289efc821356fe8efed
Paulo Krieger <paulokrieger@gmail.com>
2020-06-12 08:20:22 -0300

    changes ok

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import { classToClass } from 'class-transformer';
 
 
 interface IRequest {
@@ -23,6 +24,7 @@ class ListProvidersService {
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
     let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
+
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         expect_user_id: user_id,
@@ -30,7 +32,7 @@ class ListProvidersService {
 
       // console.log('A query no banco foi feita!');
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
 
     return users;
